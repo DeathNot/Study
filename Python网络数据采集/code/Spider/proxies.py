a34a8bde18c3a7b71e8169654237b027aea45836
DeathNot <725509787@qq.com>
2018-02-18 16:36:17 +0800

    proxies

---
+++ Python网络数据采集/code/Spider/proxies.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+class IP():
+    
+    def __init__(self):
+        self.start_url = 'http://www.ip3389.com/ih/'
+        self.headers = {
+            'Accept':r'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language':'zh-CN,zh;q=0.9',
+            'User-Agent':r'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'}
+    
+    def getHtmlText(self, url, code='utf-8'):
+        try:
+            r = requests.get(url, headers=self.headers, timeout=30)
+            r.raise_for_status()
+            r.encoding = code
+            return r.text
+        except:
+            return ''
+    
+    def getPageNum(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        pageNum = soup.find_all('a',{'class':'num'})[-1].text
+        return int(pageNum)
+        
+    def getIPList(self, num):
+        url = self.start_url + 'p' + str(num) + '/'
+        html = self.getHtmlText(url)
+        soup = BeautifulSoup(html, 'html.parser')
+        ip_list = soup.find('table', {'id':'ip_list'})
+        trs = ip_list.find_all('tr')
+        for tr in trs:
+            try:
+                td = tr.find_all('td')
+                yield td[2].text + ':' + td[3].text
+            except:
+                continue
+    
+    def saveIP(self, ipLst):
+        root = 'E:\\ip.txt'
+        with open(root, 'a') as f:
+            for ip in ipLst:
+                try:
+                    f.write(ip + '\n')
+                except:
+                    continue
+        f.close() 
+    
+    def run(self, nu=100):
+        for i in range(1, nu+1):
+            time.sleep(3)
+            try:
+                ipLst = self.getIPList(i)
+                self.saveIP(ipLst)        
+            except:
+                continue
+
+if __name__ == '__main__':
+    ip = IP()
+    ip.run()
+            
+        
+        
+            
